854977b97c750e3c5c6f609ebe885c1faaf795ac
Syndr Air1 <syndrair1@Syndrs-MacBook-Air.local>
2024-06-20 04:18:06 +0530

    remove build folder

--- build/utils/maths.js
+++
@@ -1,8 +0,0 @@
-export function decimalStringToBigInt(decimalStr, precision) {
-    const [integerPart, fractionalPart] = decimalStr.split(".");
-    const combined = integerPart + (fractionalPart || "");
-    const bigIntValue = BigInt(combined);
-    return (bigIntValue *
-        BigInt(10) **
-            BigInt(precision - (fractionalPart ? fractionalPart.length : 0)));
-}
